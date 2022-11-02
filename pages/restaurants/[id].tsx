94ab79eb3f94ec4a634aacec3b35a51f0b86e60e
Elias Consalvo <econs013@fiu.edu>
2022-11-02 15:30:53 -0400

    added dishes images

--- pages/restaurants/[id].tsx
+++ pages/restaurants/[id].tsx
@@ -1,11 +1,12 @@
-
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GetServerSideProps } from "next";
-import Model from "../../models/Restaurants";
+import restaurantModel from "../../models/Restaurants";
 import connectMongo from "../../controller/connectDB";
 import awsHelper from "../../controller/awsURL";
-import Image from 'next/image'
+import dishModel from "../../models/Dishes";
+import styles from "../../styles/UniqueRest.module.css";
+import { read } from "fs";
 
 type Restaurant = {
   name: string;
@@ -14,29 +15,79 @@ type Restaurant = {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  
-  const {id} = context.query;
+  const { id } = context.query;
   await connectMongo();
-  const restaurant = await Model.findById({_id: id});
-  if(restaurant != null) {
+  const restaurant = await restaurantModel.findById({ _id: id });
+  if (restaurant != null) {
     restaurant._id = restaurant._id.toString();
   }
   const url = await awsHelper(restaurant._id.toString());
-  
+
+  const dishes = await dishModel.find({ restaurant: restaurant.name });
+
   return {
-    props: {restaurant: JSON.parse(JSON.stringify(restaurant)), url: url},
+    props: {
+      restaurant: JSON.parse(JSON.stringify(restaurant)),
+      url: url,
+      dishes: JSON.parse(JSON.stringify(dishes)),
+    },
   };
 };
 
-const Restaurant: NextPage = ({restaurant, url}:any) => {
+const Restaurant: NextPage = ({ restaurant, url, dishes }: any) => {
+  // Set the name of the dish on S3 by their unique ID
+  // Get dishes from db by restaurant id
+  // Set image to each dish
+  // Print dishes
+
   const [rest, setRest] = useState<Restaurant[]>([]);
+  const [ready, setReady] = useState(false);
+
+  const compStyle = {
+    width: '200px',
+    height: '200px'
+  }
+
+  useEffect(() => {
+    dishes.map(async (dish: any, index: any) => {
+      const response = await getDishImg(dish._id);
+      dishes[index].url = response.url;
+     
+      setReady(true);
+    });
+  }, []);
+
+  const getDishImg = async (item: Number) => {
+    const resp = await fetch(`/api/urls/${item}`);
+    const url = await resp.json();
+    console.log(resp.status);
+    return url;
+  };
 
   return (
     <div>
-      
-      <img src={url}/>
       <h1>{restaurant.name}</h1>
-      
+      <img src={url} />
+      <h2>Dishes</h2>
+
+      {ready ? (
+        <div>
+          {dishes.map((dish: any, index: any) => {
+            return (
+              <div key={index}>
+                <div>Name:{dish.name}</div>
+                <img src={dish.url} style={compStyle}/>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className={styles.loader}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      )}
     </div>
   );
 };
